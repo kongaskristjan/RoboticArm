c63f5605804d0e2b0c57440bdd385304668d7172
Kristjan Kongas <kongaskristjan@gmail.com>
2019-07-04 16:52:36 +0300

    Add runRemote.sh

---
+++ runRemote.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+ADDR=pi@192.168.43.26
+DIR=/home/pi/src/python/RoboticArm/
+PROGRAM=./control.py
+echo "Copying this directory to $ADDR:$DIR and launching $PROGRAM remotely"
+
+scp * $ADDR:$DIR
+ssh $ADDR $DIR$PROGRAM
