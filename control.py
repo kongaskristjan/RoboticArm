0cccc986026ab3f4758abec4153299fac93bb197
Kristjan Kongas <kongaskristjan@gmail.com>
2019-07-04 18:09:52 +0300

    Partial: add servo control

--- control.py
+++ control.py
@@ -1,3 +1,18 @@
 #!/usr/bin/python3
 
-print("Hello")
+import servoLib
+import math, time
+
+def main():
+    near = servoLib.Servo(15)
+    far = servoLib.Servo(14)
+
+    while True:
+        near.setPosition(math.pi / 2)
+        time.sleep(1)
+        near.setPosition(-math.pi / 2)
+        time.sleep(1)
+
+
+if __name__ == "__main__":
+    main()
