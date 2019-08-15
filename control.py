705f5c0e726131fddaee25255e6d2c178fcca79b
Kristjan Kongas <kongaskristjan@gmail.com>
2019-08-15 23:05:46 +0300

    Add command line interface for custom text and test flag

--- control.py
+++ control.py
@@ -2,17 +2,18 @@
 
 import arm, time, fire
 
-def main(speed=1):
+def main(speed=1, text="Hello world!", test=False):
     speedActive = 5 * speed
     speedInactive = 16 * speed
     letterSleep = 0.5 / speed
 
     lineDrawer = arm.Arm()
-    #lineDrawer.setStraight()
-    #time.sleep(5)
-    #lineDrawer.drawText("ABCDEFGHIJKLMNOPQRSTUVWXYZ", speedActive, speedInactive, letterSleep)
-    #lineDrawer.drawText("., !?", speedActive, speedInactive, letterSleep)
-    lineDrawer.drawText("Tere Liisi, Madli, Kristjan, Lauri, Rutt, Olav ja kassid!", speedActive, speedInactive, letterSleep)
+    if test:
+        lineDrawer.setStraight()
+        time.sleep(2)
+    else:
+        lineDrawer.drawText(str(text), speedActive, speedInactive, letterSleep)
+
 
 if __name__ == "__main__":
     try:
